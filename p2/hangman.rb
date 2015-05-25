b86119a6f93e6ff23363bd263adbbc5901082e33
Dipen Chauhan <get.dipen@gmail.com>
2015-05-25 11:34:55 -0700

    Read words file

--- p2/hangman.rb
+++ p2/hangman.rb
@@ -8,6 +8,20 @@
 # 
 
 $word_size = 5
+$words = Array.new
+
+def play
+	f = File.open("words")
+	f.each do |line|
+		if (line.size == $word_size + 1)
+			$words << line
+		end
+	end
+end
+
+def draw
+
+end
 
 puts "+-------------+"
 puts "|   HANGMAN   |"
@@ -18,3 +32,4 @@ if (($word_size < 5) || ($word_size > 20))
 	$word_size = 5
 end
 puts "word size = " + $word_size.to_s
+play
