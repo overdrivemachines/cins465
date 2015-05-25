94831a80dcaa1baedb16d087d2439540ba4d4823
Dipen Chauhan <get.dipen@gmail.com>
2015-05-25 12:33:49 -0700

    Ask user to input guess

--- p2/hangman.rb
+++ p2/hangman.rb
@@ -11,12 +11,27 @@ $word_size = 5
 $words = Array.new
 
 def play
+	letters_right = Array.new
+	letters_wrong = Array.new
+	
+	# Read the file and add only the words of the
+	# specified size into the array
 	f = File.open("words")
 	f.each do |line|
 		if (line.size == $word_size + 1)
 			$words << line
 		end
 	end
+
+	# Ask user to input guess
+	print "Enter your guess: "
+	letter = gets.chomp
+	rx = Regexp.new("^[a-z]$")	
+	while ((rx =~ letter) != 0) do
+		print "Enter your guess (a-z only): "
+		letter = gets.chomp
+	end
+
 end
 
 def draw
