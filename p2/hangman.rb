00d82d26aabf390f6f319ddd0e95026ef6988e19
Dipen Chauhan <get.dipen@gmail.com>
2015-05-24 18:09:20 -0700

    Determined word size

--- p2/hangman.rb
+++ p2/hangman.rb
@@ -6,3 +6,15 @@
 # 
 # Dipen Chauhan
 # 
+
+$word_size = 5
+
+puts "+-------------+"
+puts "|   HANGMAN   |"
+puts "+-------------+"
+print "Enter word length between 5-20: "
+$word_size = gets.chomp.to_i
+if (($word_size < 5) || ($word_size > 20))
+	$word_size = 5
+end
+puts "word size = " + $word_size.to_s
