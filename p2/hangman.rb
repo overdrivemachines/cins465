ae5efe02a6c8e89ebc87140f459025c8ff795203
Dipen Chauhan <get.dipen@gmail.com>
2015-05-27 22:29:57 -0700

    Removing words from array

--- p2/hangman.rb
+++ p2/hangman.rb
@@ -23,21 +23,27 @@ def play
 		end
 	end
 
-	# Ask user to input guess
-	print "Enter your guess: "
-	letter = gets.chomp
-	rx = Regexp.new("^[a-z]$")	
-	while ((rx =~ letter) != 0) do
-		print "Enter your guess (a-z only): "
+	while (true) do
+		# Ask user to input guess
+		print "Enter your guess: "
 		letter = gets.chomp
+		rx = Regexp.new("^[a-z]$")	
+		while ((rx =~ letter) != 0) do
+			print "Enter your guess (a-z only): "
+			letter = gets.chomp
+		end
+
+		# puts "# of words = " + $words.size.to_s
+		# Check for hit or miss
+		if (hit?(letter) == true)
+			print "Hit. "
+		else
+			print "Miss. "
+		end
+		puts "# of words in Array = " + $words.size.to_s
 	end
 
-	# Check for hit or miss
-	if (hit?(letter) == true)
-		puts "hit"
-	else
-		puts "miss"
-	end
+	
 end
 
 # Returns true when letter matches with one of the letters of 
@@ -53,14 +59,30 @@ def hit?(letter)
 		end
 	end
 
+	puts matches.to_s + " words contain the letter " + letter + ". Removing them.."
+
 	if ((matches > 1) || (matches == 0))
 		# Remove all the words from the array
-		# that contain letter
+		# that contain the letter
+		removeWords(rx)
+		# matches = 0
+		# $words.each do |word|
+		# 	if ((rx =~ word) != nil)
+		# 		$words.delete(word)
+		# 		matches += 1
+		# 	end
+		# end
+		# puts "Removed " + matches.to_s + " words."
+
+		matches = 0
+		rx = Regexp.new(letter)
 		$words.each do |word|
 			if (rx.match(word) != nil)
-				$words.delete(word)
+				# print word + " "
+				matches += 1
 			end
 		end
+		puts matches.to_s + " words contain the letter " + letter
 		return false
 	else
 		# Remove all words from the array that
@@ -71,6 +93,31 @@ def hit?(letter)
 			end
 		end
 		return true
+	end
+end
+
+def removeWords(expression = Regexp.new(""))
+	matches = 0
+	for i in 0..$words.size
+		word = $words[i]
+		if ((expression =~ word) != nil)
+			matches += 1
+			$words[i] = "0"
+		end
+	end
+	$words.delete("0")
+	puts "Number of words removed = " + matches.to_s
+	
+	matches = 0
+	
+	$words.each do |word|
+		if ((expression =~ word) != nil)
+			matches += 1
+		end
+	end
+
+	if (matches > 0)
+		removeWords(expression)
 	end
 end
 
