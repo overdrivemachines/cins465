01a022128b84ea437ce9f0d74e8f95b5a2480c3e
Dipen Chauhan <get.dipen@gmail.com>
2015-06-12 13:09:33 -0700

    Randomly selected start and end word

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
+load "#{Rails.root}/lib/ladder.rb"
+
 class HomeController < ApplicationController
 	def index
+		dsize = dictionary.size
+		@start_word = dictionary[rand(dsize)]
+		@end_word = dictionary[rand(dsize)]
 	end
 
 	def result
