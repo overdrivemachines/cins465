8654a871e7de375333bb9959f524f1db6e85ce88
Dipen Chauhan <get.dipen@gmail.com>
2015-06-14 20:00:05 -0700

    Reading params

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -8,5 +8,19 @@ class HomeController < ApplicationController
 	end
 
 	def result
+		start_word = params[:start_word]
+		end_word = params[:end_word]
+		word1 = params[:word1]
+		word2 = params[:word2]
+		word3 = params[:word3]
+		word4 = params[:word4]
+		word5 = params[:word5]
+
+		@res = false
+		if (edit_distance_of_one(start_word, word1))
+			@res = true
+		else
+			@res = false
+		end
 	end
 end
