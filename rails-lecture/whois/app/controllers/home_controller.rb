eda8440c82a2f3a7d3e83bcfd5aece364d16c474
Dipen Chauhan <get.dipen@gmail.com>
2015-06-26 18:04:35 -0700

    Initial commit

---
+++ rails-lecture/whois/app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+	def index
+	end
+
+	def show
+		@query_text = params[:query]
+		if @query_text =~ /^[a-zA-Z0-9\.\-]*$/
+			@result = `whois #{@query_text}`.split("\n")
+		else
+			@result = ["Illegal URL"];
+		end
+	end
+end
---
+++ rails-lecture/whois/config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  get 'query', to: 'home#show'
+end
