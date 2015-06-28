cab1ca3c61368b1ef59701793ccca485416903ed
Dipen Chauhan <get.dipen@gmail.com>
2015-06-27 21:04:09 -0700

    Added new and create methods

--- rails-lecture/scorebook/app/controllers/high_scores_controller.rb
+++ rails-lecture/scorebook/app/controllers/high_scores_controller.rb
@@ -5,4 +5,15 @@ class HighScoresController < ApplicationController
 	def show
 		@high_score = HighScore.find(params[:id])
 	end
+	def new
+		@high_score = HighScore.new
+	end
+	def create
+		@high_score = HighScore.new(params.require(:high_score).permit(:user, :game, :score))
+		if (@high_score.save)
+			redirect_to @high_score, notice: 'High Score was successfully created'
+		else
+			render :new
+		end
+	end
 end
