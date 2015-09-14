ec7b9d3f1e83873b02d40c6fdd1e45f8d402fc99
Dipen Chauhan <get.dipen@gmail.com>
2015-09-14 16:43:39 -0700

    Initial commit

---
+++ rails-lecture/ratings/db/migrate/20150914234110_create_ratings.rb
@@ -0,0 +1,11 @@
+class CreateRatings < ActiveRecord::Migration
+  def change
+    create_table :ratings do |t|
+      t.string :course
+      t.string :comment
+      t.references :professor, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
