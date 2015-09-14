1cf98c1cb915b5849df18ab9ed088727cb290bfc
Dipen Chauhan <get.dipen@gmail.com>
2015-09-14 12:48:25 -0700

    Using the routes resources function

--- rails-lecture/scorebook/config/routes.rb
+++ rails-lecture/scorebook/config/routes.rb
@@ -1,24 +1,5 @@
 Rails.application.routes.draw do
-	# Create a new HighScore
-	get 'high_scores/new', to: 'high_scores#new', as: 'new_high_score'
-
-	# Edit an existing HighScore
-	get 'high_scores/:id/edit', to: 'high_scores#edit', as: 'edit_high_score'
-
-	# Show a single object
-	get 'high_scores/:id', to: 'high_scores#show', as: 'high_score'
-	
-	# All HighScore objects
-	get 'high_scores', to: 'high_scores#index', as: 'high_scores'
-
-	# Object is created
-	post 'high_scores', to: 'high_scores#create'
-
-	# Object is updated
-	patch 'high_scores/:id', to: 'high_scores#update'
-
-	# Delete the object
-	delete 'high_scores/:id', to: 'high_scores#destroy'
+	resources :high_scores
 
 	root 'high_scores#index'
 end
