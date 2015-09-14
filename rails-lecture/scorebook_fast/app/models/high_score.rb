c03e22708667b5e715eb800741ddfc3fb1dfcd35
Dipen Chauhan <get.dipen@gmail.com>
2015-09-14 13:11:53 -0700

    Added validation

--- rails-lecture/scorebook_fast/app/models/high_score.rb
+++ rails-lecture/scorebook_fast/app/models/high_score.rb
@@ -1,2 +1,5 @@
 class HighScore < ActiveRecord::Base
+	validates :user, presence: true
+	validates :game, presence: true
+	validates :score, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
