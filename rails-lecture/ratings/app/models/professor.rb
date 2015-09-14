eda59e0b9d2117dc6aa961e534b23a28c19bfefd
Dipen Chauhan <get.dipen@gmail.com>
2015-09-14 16:59:47 -0700

    Added annotate

--- rails-lecture/ratings/app/models/professor.rb
+++ rails-lecture/ratings/app/models/professor.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: professors
+#
+#  id         :integer          not null, primary key
+#  first      :string
+#  last       :string
+#  university :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Professor < ActiveRecord::Base
 end
--- rails-lecture/ratings/app/models/rating.rb
+++ rails-lecture/ratings/app/models/rating.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: ratings
+#
+#  id           :integer          not null, primary key
+#  course       :string
+#  comment      :string
+#  professor_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Rating < ActiveRecord::Base
   belongs_to :professor
 end
