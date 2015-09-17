d3bfc85b5c669cd3e68854f429e945c00286fd17
Dipen Chauhan <get.dipen@gmail.com>
2015-09-16 22:41:42 -0700

    Defined new create update and destroy

--- rails-lecture/ratings/app/controllers/ratings_controller.rb
+++ rails-lecture/ratings/app/controllers/ratings_controller.rb
@@ -4,21 +4,53 @@ class RatingsController < ApplicationController
 	# GET /professors/:professor_id/ratings
 	def index
 		@professor = Professor.find(params[:professor_id])
-		@ratings = Rating.all
+		@ratings = @professor.ratings
 	end
 
+	# GET /professors/:professor_id/ratings/new
 	def new
+		@professor = Professor.find(params[:professor_id])
+		@rating = @professor.ratings.new
 	end
 
+	# GET /ratings/1/edit
+	# set_ratings called
 	def edit
 	end
 
+	# POST /professors/:professor_id/ratings
 	def create
+		@professor = Professor.find(params[:professor_id])
+		@rating = @professor.ratings.new(rating_params)
+		if @rating.save
+			redirect_to professor_ratings_url(@professor), notice: 'Rating was successfully created.'
+		else
+			render :new
+		end
 	end
 
+	# PUT/PATCH /ratings/1
+	# set_ratings called
 	def update
+		if @ratings.update(rating_params)
+			redirect_to professor_ratings_url(@professor), notice: 'Rating was successfully updated.'
+		else
+			render :edit
+		end
 	end
 
+	# DELETE /ratings/1
+	# set_ratings called
 	def destroy
-	end	
+		@rating.destroy
+		redirect_to professor_ratings_url(@rating.professor), notice: 'Rating was successfully destroyed.'
+	end
+
+	private
+		def set_rating
+			@rating = Rating.find(params[:id])
+		end
+
+		def rating_params
+		end
 end
