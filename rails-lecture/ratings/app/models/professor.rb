067283c31a207905d6615ca29c4fb6988c7e6074
Dipen Chauhan <get.dipen@gmail.com>
2015-09-22 13:45:24 -0700

    Reject nested attributes when they are empty

--- rails-lecture/ratings/app/models/professor.rb
+++ rails-lecture/ratings/app/models/professor.rb
@@ -12,7 +12,7 @@
 
 class Professor < ActiveRecord::Base
 	has_many :ratings, dependent: :destroy
-	accepts_nested_attributes_for :ratings
+	accepts_nested_attributes_for :ratings, reject_if: lambda { |attributes| attributes['course'].blank? || attributes['comment'].blank? || attributes['rating'].blank?}
 
 	validates :first, presence: true
 	validates :last, presence: true
